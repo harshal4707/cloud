44804662d0dc404ac5b1878975497f10e055774a
harshal <harshal4707@gmail.com>
2019-10-15 09:21:58 +0530

    Fibonacci series 1

---
+++ fibonaci.java
@@ -0,0 +1,15 @@
+class fibonaci
+{
+	public static void main(String args)
+	{
+		int n1=0,n2=1,n3;
+		System.out.println(n1+ "\n "+n2);
+		for(int i=2;i<20;i++)
+		{
+			n3=n1+n2;
+			System.out.println(n3);
+			n2=i;
+			n1=n3;
+		}
+	}
+}
